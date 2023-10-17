aff20e3fd819e751e91f743d98bf067be5a6b92a
Guillaume Laubier <guillaume.laubier@hotmail.fr>
2023-10-17 15:37:43 +0200

    Instream SDK updated to version 7.23.2

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SVSVideoKit",
-            url: "https://sdk.sascdn.com/mobile/instreamsdk/ios/SVSVideoKit-7.23.1.zip",
-            checksum: "531374c7ab1304c6a599933994c33d3ac5ea3320cdb300174a8c0d91dfe35d9e"),
+            url: "https://sdk.sascdn.com/mobile/instreamsdk/ios/SVSVideoKit-7.23.2.zip",
+            checksum: "07231e11291d476352d899c473e9f28e6854b291a20ac6df86e9bbd1c5a1da67"),
     ]
 )
