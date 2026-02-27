a25b5723aa9da086765960c89e7e499e34b8e2ce
Loïc GIRON DIT METAZ <lgironditmetaz@smartadserver.com>
2026-02-27 14:22:10 +0100

    Instream SDK updated to version 7.27.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SVSVideoKit",
-            url: "https://sdk.sascdn.com/mobile/instreamsdk/ios/SVSVideoKit-7.26.0.zip",
-            checksum: "e3fd8bbbdef7209054321a413f9067518a963aea20d6ffbb2bb7f5f2b88a9a9a"),
+            url: "https://sdk.sascdn.com/mobile/instreamsdk/ios/SVSVideoKit-7.27.0.zip",
+            checksum: "6e2c4b655bb586423ad3e9bab74362c9e75541d133794b849c98591a34b4a239"),
     ]
 )
